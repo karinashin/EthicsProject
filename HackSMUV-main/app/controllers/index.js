4ef94e46d25c150aa876adf82ce5c00b441166e5
karinashin <92281567+karinashin@users.noreply.github.com>
2023-11-06 13:02:02 -0600

    Initial Commit

---
+++ HackSMUV-main/app/controllers/index.js
@@ -0,0 +1,6 @@
+const auth = require("./auth");
+const tutorial = require("./tutorial");
+const home = require("./home");
+const karina = require("./karina");
+
+module.exports = { auth, tutorial, home, karina };
